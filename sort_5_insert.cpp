8ed432e7f708e3b5ee91de800ed8449db8e19849
11yyaa <113608806+11yyaa@users.noreply.github.com>
2023-03-23 14:02:40 +0300

    Add files via upload

---
+++ sort_5_insert.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void insertionSort(int list[], int listLength)
+{
+    int counter = 0;
+	for(int i = 1; i < listLength; i++)
+	{
+		int j = i - 1;
+		const int N = 100;
+		while(j >= 0 && list[j] > list[j + 1])
+		{
+			swap(list[j], list[j + 1]);
+			j--;
+			for(int i=0;i<N;i++){
+                    cout << list[i] << endl;
+                    string FileName = to_string(counter)+".txt";
+                ofstream file (FileName);
+                for(int k=0;k<N;k++){
+                    cout << list[k] << endl;
+                    file << list[k] << endl;
+                }
+                file.close();
+                counter ++;
+			}
+		}
+	}
+	string FileName = "amount.txt";
+                    std::ofstream file (FileName);
+                        {
+                        file << counter;
+                        file.close();
+                        }
+}
+
+int main()
+{
+	const int N=100;
+    int list[N],i,tmp;
+    //randomize();
+
+    for(i=0;i<N;i++) {
+        list[i] = rand()%100;
+        cout<<list[i]<<" ";
+    }
+    cout<<endl;
+
+	insertionSort(list, N);
+
+	return 0;
+}
