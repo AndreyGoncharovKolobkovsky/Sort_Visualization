70a2cdbf98bbd443a780ee3116876bf6396c1f1b
11yyaa <113608806+11yyaa@users.noreply.github.com>
2023-03-23 13:21:21 +0300

    Add files via upload

---
+++ sort_3_bubbles.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void bubbleSort(int list[], int listLength)
+{
+    int counter = 0;
+	while(listLength--)
+	{
+		bool swapped = false;
+
+		for(int i = 0; i < listLength; i++)
+		{
+			if(list[i] > list[i + 1])
+			{
+				swap(list[i], list[i + 1]);
+				swapped = true;
+				const int N=100;
+				string FileName = to_string(counter)+".txt";
+                ofstream file (FileName);
+                for(int k=0;k<N;k++){
+                    cout << list[k] << endl;
+                    file << list[k] << endl;
+                }
+                file.close();
+                counter ++;
+			}
+		}
+
+		if(swapped == false)
+			break;
+	}
+	string FileName = "amount.txt";
+        ofstream file (FileName);
+        {
+        file << counter;
+        file.close();
+        }
+}
+
+int main()
+{
+	const int N=100;
+    int list[N],i,tmp;
+    //randomize();
+
+    for(i=0;i<N;i++) {
+        list[i] = rand()%100;
+        cout<<list[i]<<" ";
+    }
+    cout<<endl;
+	bubbleSort(list, N);
+
+	//cout << "Sorted array ..." << endl;
+	//for(int i = 0; i < N; i++)
+		//cout << list[i] << '\t';
+	//cout << endl;
+}
